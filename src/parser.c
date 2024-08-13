540e1cfc33247a29305ed78c26d5211120f42af1
Ellen Eklund <eeklund@f1r1s13.codam.nl>
2024-08-13 18:59:06 +0200

    continued with parser, making the logic good

--- src/parser.c
+++ src/parser.c
@@ -1,92 +1,227 @@
-#include "minishell.h"
-#include <stdlib.h>
-#include <unistd.h>
-#include "tokenizer.h"
+#include "parser.h"
 
-
-char *find_command_path(char *command)
+t_command	*init_cmd(void)
 {
-    char *path = getenv("PATH");
-    char *path_copy = ft_strdup(path);
-    //illeagal strtok!!!
-    char *dir = strtok(path_copy, ":");
-    char *full_path = NULL;
+	t_command	*cmd;
 
-    while (dir != NULL) {
-        full_path = malloc(strlen(dir) + ft_strlen(command) + 2);
-        sprintf(full_path, "%s/%s", dir, command);
-        
-        if (access(full_path, X_OK) == 0) {
-            free(path_copy);
-            return full_path;
-        }
-        
-        free(full_path);
-        dir = strtok(NULL, ":");
-    }
-
-    free(path_copy);
-    return ft_strdup(command);  // Return the command as-is if not found in PATH
+	cmd = malloc(sizeof(t_command));
+	if (!cmd)
+		return (NULL);
+	cmd->argv = malloc(sizeof(char *) * (MAX_ARGS + 1));
+	if (!cmd->argv)
+	{
+		free(cmd);
+		return (NULL);
+	}
+	cmd->redirects = malloc (sizeof(t_redirect) * MAX_REDIRECTS);
+	if (!cmd->argv)
+	{
+		free(cmd->argv);
+		free(cmd);
+		return (NULL);
+	}
+	cmd->redirect_count = 0;
+	cmd->pipe_in = STDIN_FILENO;
+	cmd->pipe_out = STDOUT_FILENO;
+	cmd->next = NULL;
+	cmd->path = NULL;
+	return (cmd);
 }
 
+// void	free_command_list(t_command *head)
 
-t_command *parse_command(char *input)
+int	is_redirect_token(int token_type)
 {
-    t_command *cmd;
-    t_token *tokens;
-    int argc = 0;
-    int i;
+	return (token_type == TOKEN_REDIRECT_IN || \
+		token_type == TOKEN_REDIRECT_OUT || \
+		token_type == TOKEN_REDIRECT_APPEND || \
+		token_type == TOKEN_HEREDOC);
+}
 
-    cmd = malloc(sizeof(t_command));
-    if (!cmd)
-        return NULL;
+int	handle_redirection_parsing(t_command *cmd, t_token **token)
+{
+	cmd->redirects[cmd->redirect_count].type = (*token)->type;
+	(*token) = (*token)->next;
+	if (token && (*token)->type == TOKEN_WORD)
+	{
+		cmd->redirects[cmd->redirect_count].file = ft_strdup((*token)->content);
+		if (!cmd->redirects[cmd->redirect_count].file)
+			return (0);
+		cmd->redirect_count++;
+		return (1);
+	}
+	return (0);
+}
 
-    // Initialize all fields to NULL or 0
-    cmd->path = NULL;
-    cmd->argv = NULL;
-    cmd->redirects = NULL;
-    cmd->redirect_count = 0;
-    cmd->pipe_in = STDIN_FILENO;
-    cmd->pipe_out = STDOUT_FILENO;
+t_command *parse_command_from_tokens(t_token *tokens)
+{
+	t_command	*head;
+	t_command	*cur_cmd;
+	t_command	*new_cmd;
+	int			i;
 
-    tokens = tokenizer(input);
-    if (!tokens)
-    {
-        free(cmd);
-        return NULL;
-    }
-    print_token_list(tokens);
-    // Count the number of word tokens to determine argc
-    for (t_token *t = tokens; t != NULL; t = t->next)
-    {
-        if (t->type == TOKEN_WORD)
-            argc++;
-    }
+	new_cmd = NULL;
+	head = init_cmd();
+	if (!head)
+		return (NULL);
+	cur_cmd = head;
+	i = 0;
+	while (tokens && i < MAX_ARGS)
+	{
+		if (tokens->type == TOKEN_WORD)
+		{
+			cur_cmd->argv[i] = ft_strdup(tokens->content);
+			if (!cur_cmd->argv[i])
+				return (NULL);
+			i++;
+		}
+		else if (is_redirect_token(tokens->type))
+		{
+			if (cur_cmd->redirect_count >= MAX_REDIRECTS)
+				return (NULL); //implement error handling
+			if (!handle_redirection_parsing(cur_cmd, &tokens))
+				return (NULL); // handle error
+		}
+		else if (tokens->type == TOKEN_PIPE)
+		{
+			cur_cmd->argv[i] = NULL;
+			cur_cmd->pipe_out = 1; // this node's output will be piped to the next node
+			new_cmd = init_cmd();
+			if (!new_cmd)
+				return (NULL); //handle error
+			cur_cmd->next = new_cmd;
+			cur_cmd = new_cmd;
+			i = 0;
+		}
+		tokens = tokens->next;
+	}
+	cur_cmd->argv[i] = NULL;
+	cur_cmd = head;
+	while (cur_cmd)
+	{
+		cur_cmd->path = ft_strdup(cur_cmd->argv[0]);
+		if (!cur_cmd->path)
+			return (NULL);
+		cur_cmd = cur_cmd->next;
+	}
+	return (head);
+}
 
-    cmd->argv = malloc(sizeof(char *) * (argc + 1));
-    if (!cmd->argv)
-    {
-        free(cmd);
-        free_tokens(&tokens);
-        return NULL;
-    }
+void	print_cmd_list(t_command *head)
+{
+	// t_command	*temp;
+	int			i;
+	int			j;
 
-    // Fill argv array
-    i = 0;
-    for (t_token *t = tokens; t != NULL; t = t->next)
-    {
-        if (t->type == TOKEN_WORD)
+	printf("inside print cmd_lst\n");
+	// temp = head;
+	while (head)
+	{
+		printf("command path-> '%s' redir count:%i\n", head->path, head->redirect_count);
+		i = 0;
+		while (head->argv && head->argv[i])
+		{
+			printf("arg: %s\n", head->argv[i]);
+			i++;
+		}
+		if (head->redirect_count != 0)
+		{
+			printf("redirecttype == %i target file == %s\n", head->redirects->type, head->redirects->file);
+		}
+	    printf("  Pipe In: %d, Pipe Out: %d\n", head->pipe_in, head->pipe_out);
+		j = 0;
+        for (j = 0; j < head->redirect_count; j++)
         {
-            cmd->argv[i] = ft_strdup(t->content);
-            i++;
+            printf("  Redirect[%d] Type: %d, File: %s\n", j, head->redirects[j].type, head->redirects[j].file);
         }
-        // Here you can handle redirections and other token types
-    }
-    cmd->argv[i] = NULL;
-
-    if (argc > 0)
-        cmd->path = find_command_path(cmd->argv[0]);
-
-    free_tokens(&tokens);
-    return cmd;
+		head = head->next;
+	}
+	printf("\n");
 }
+
+// char *find_command_path(char *command)
+// {
+//     char *path = getenv("PATH");
+//     char *path_copy = ft_strdup(path);
+//     //illeagal strtok!!!
+//     char *dir = strtok(path_copy, ":");
+//     char *full_path = NULL;
+
+//     while (dir != NULL) {
+//         full_path = malloc(strlen(dir) + ft_strlen(command) + 2);
+//         sprintf(full_path, "%s/%s", dir, command);
+        
+//         if (access(full_path, X_OK) == 0) {
+//             free(path_copy);
+//             return full_path;
+//         }
+        
+//         free(full_path);
+//         dir = strtok(NULL, ":");
+//     }
+
+//     free(path_copy);
+//     return ft_strdup(command);  // Return the command as-is if not found in PATH
+// }
+
+
+// t_command *parse_command(char *input)
+// {
+//     t_command *cmd;
+//     t_token *tokens;
+//     int argc = 0;
+//     int i;
+
+//     cmd = malloc(sizeof(t_command));
+//     if (!cmd)
+//         return NULL;
+
+//     // Initialize all fields to NULL or 0
+//     cmd->path = NULL;
+//     cmd->argv = NULL;
+//     cmd->redirects = NULL;
+//     cmd->redirect_count = 0;
+//     cmd->pipe_in = STDIN_FILENO;
+//     cmd->pipe_out = STDOUT_FILENO;
+
+//     tokens = tokenizer(input);
+//     if (!tokens)
+//     {
+//         free(cmd);
+//         return NULL;
+//     }
+//     print_token_list(tokens);
+//     // Count the number of word tokens to determine argc
+//     for (t_token *t = tokens; t != NULL; t = t->next)
+//     {
+//         if (t->type == TOKEN_WORD)
+//             argc++;
+//     }
+
+//     cmd->argv = malloc(sizeof(char *) * (argc + 1));
+//     if (!cmd->argv)
+//     {
+//         free(cmd);
+//         free_tokens(&tokens);
+//         return NULL;
+//     }
+
+//     // Fill argv array
+//     i = 0;
+//     for (t_token *t = tokens; t != NULL; t = t->next)
+//     {
+//         if (t->type == TOKEN_WORD)
+//         {
+//             cmd->argv[i] = ft_strdup(t->content);
+//             i++;
+//         }
+//         // Here you can handle redirections and other token types
+//     }
+//     cmd->argv[i] = NULL;
+
+//     if (argc > 0)
+//         cmd->path = find_command_path(cmd->argv[0]);
+
+//     free_tokens(&tokens);
+//     return cmd;
+// }
