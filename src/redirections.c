a858e2a957cc54d681cf531cc7f78b94aaac7fcb
Ellen Eklund <eeklund@f1r1s17.codam.nl>
2024-08-20 18:17:08 +0200

    redir start

--- src/redirections.c
+++ src/redirections.c
@@ -2,10 +2,14 @@
 
 void setup_redirections(t_command *cmd)
 {
+	int	new_fd;
     // Placeholder function, implement redirection logic here
 	if (cmd->redirect_count == 0)
 		return ;
-	
+	new_fd = open(cmd->redirects->file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+			
+
+
     (void)cmd;
     // printf("DEBUG: setup_redirections called\n");
 }
