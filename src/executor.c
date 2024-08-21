52d85abda124fda099fa196a28f723f68ccd10c9
Ellen Eklund <eeklund@f0r4s3.codam.nl>
2024-08-21 18:01:32 +0200

    continued on redirections

--- src/executor.c
+++ src/executor.c
@@ -6,7 +6,7 @@
 /*   By: eeklund <eeklund@student.42.fr>              +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/08/13 18:15:38 by eeklund       #+#    #+#                 */
-/*   Updated: 2024/08/19 19:25:52 by eeklund       ########   odam.nl         */
+/*   Updated: 2024/08/21 11:06:36 by eeklund       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -168,52 +168,52 @@ char *find_executable(char *command)
 
 void execute_command(char *command)
 {
-    t_token *tokens;
-    t_command *cmd;
-	char	*path;
-    pid_t pid;
-    int status;
-    printf("command: %s\n", command);
-    tokens = tokenizer(command);
-    if (!tokens)
-        return;
+	t_token		*tokens;
+	t_command	*cmd;
+	char		*path;
+	pid_t		pid;
+	int			status;
 
-    cmd = parse_command_from_tokens(tokens);
-    if (!cmd)
-    {
-        free_tokens(&tokens);
-        return;
-    }
-    print_cmd_list(cmd);
-    if (is_builtin(cmd->argv[0]))
-    {
-        execute_builtin(cmd);
-    }
-    else
-    {
-        pid = fork();
-        if (pid == -1)
-        {
-            perror("minishell: fork");
-        }
-        else if (pid == 0)
-        {
-            setup_redirections(cmd);
-            // printf("Executing external command: %s\n", cmd->argv[0]);
-            // execvp(cmd->argv[0], cmd->argv);
-            printf("execve\n");
+	printf("command: %s\n", command);
+	tokens = tokenizer(command);
+	if (!tokens)
+		return ;
+
+	cmd = parse_command_from_tokens(tokens);
+	if (!cmd)
+	{
+		free_tokens(&tokens);
+		return ;
+	}
+	// print_cmd_list(cmd);
+	if (is_builtin(cmd->argv[0]))
+		execute_builtin(cmd);
+	else
+	{
+		pid = fork();
+		if (pid == -1)
+		{
+			perror("minishell: fork");
+		}
+		else if (pid == 0)
+		{
+			setup_redirections(cmd);
+			// if (ft_strcmp(cmd->argv[0], "echo") == 0)
+			// {
+			// 	// printf("DEBUG: Executing 'echo' builtin\n");
+			// 	builtin_echo(cmd->argv);
+			// }
 			path = find_executable(cmd->argv[0]);
-            execve(path, cmd->argv, environ);
-            perror("minishell: execvp failed\n");
-            exit(EXIT_FAILURE);
-        }
-        else
-        {
-            waitpid(pid, &status, 0);
-            update_exit_status(status);
-        }
-    }
-
-    free_command(cmd);
-    free_tokens(&tokens);
+			execve(path, cmd->argv, environ);
+			perror("minishell: execve failed\n");
+			exit(EXIT_FAILURE);
+		}
+		else
+		{
+			waitpid(pid, &status, 0);
+			update_exit_status(status);
+		}
+	}
+	free_command(cmd);
+	free_tokens(&tokens);
 }
--- src/redirections.c
+++ src/redirections.c
@@ -1,15 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   redirections.c                                     :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: eeklund <eeklund@student.42.fr>              +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2024/08/21 11:00:43 by eeklund       #+#    #+#                 */
+/*   Updated: 2024/08/21 15:52:29 by eeklund       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
-void setup_redirections(t_command *cmd)
+void	setup_redirections(t_command *cmd)
 {
-	int	new_fd;
-    // Placeholder function, implement redirection logic here
-	if (cmd->redirect_count == 0)
-		return ;
-	new_fd = open(cmd->redirects->file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-			
+	int	fd;
+	int	cur;
 
+	cur = 0;
+	while (cur < cmd->redirect_count)
+	{
+		if (cmd->redirects[cur].type == TOKEN_REDIRECT_OUT)
+			fd = open(cmd->redirects[cur].file, O_WRONLY | O_CREAT | O_TRUNC, 420);
+		else if (cmd->redirects[cur].type == TOKEN_REDIRECT_APPEND)
+			fd = open(cmd->redirects[cur].file, O_WRONLY | O_CREAT | O_APPEND, 420);
+		else if (cmd->redirects[cur].type == TOKEN_REDIRECT_IN)
+			fd = open(cmd->redirects[cur].file, O_RDONLY);
+		if (fd == -1)
+		{
+			perror("Error opening file for redirection");
+			return ;
+		}
+		if ((cur == cmd->redirect_count - 1)  && (cmd->redirects[cur].type == TOKEN_REDIRECT_OUT || cmd->redirects[cur].type == TOKEN_REDIRECT_APPEND))
+		{
+			if (dup2(fd, STDOUT_FILENO) == -1)
+			{
+				perror("Error redirecting stdout");
+				close(fd);
+				return ;
+			}
+		}
+		else if (cmd->redirects[cur].type == TOKEN_REDIRECT_IN)
+		{
+			if (dup2(fd, STDIN_FILENO) == -1)
+			{
+				perror("Error redirecting stdout");
+				close(fd);
+				return ;
+			}
+		}
+		close(fd);
+		cur++;
+	}
 
-    (void)cmd;
-    // printf("DEBUG: setup_redirections called\n");
+	// if (cmd->redirects->type == TOKEN_HEREDOC)
+	// {
+	// 	handle_heredoc(cmd);
+	// }
+
+	// printf("DEBUG: setup_redirections called\n");
 }
