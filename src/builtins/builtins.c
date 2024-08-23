6a4c001b8e1bbecc1b3bef13cf0bf128ef4fc1f1
roman lodeizen <rlodeize@student.codam.nl>
2024-08-23 16:49:29 +0200

    bultins in parent or child

--- src/builtins/builtins.c
+++ src/builtins/builtins.c
@@ -3,11 +3,98 @@
 #include <string.h>
 #include <stdio.h>
 
-// Array of builtin command names
-static const char *builtin_commands[] = {
-    "cd", "pwd", "export", "unset", "env", "exit", "bye", NULL
+
+
+//rework parent process to handle builtins and externals
+// # Commands that must be executed in the parent process:
+
+// 1. cd (change directory)
+// 2. export (set environment variables)
+// 3. unset (unset environment variables)
+// 4. exit (terminate the shell)
+// 5. source or . (execute commands from a file in the current shell)
+// 6. alias (define or display aliases)
+// 7. unalias (remove aliases)
+// 8. set (set or unset shell options and positional parameters)
+// 9. jobs (display status of jobs in the current session)
+// 10. fg (move job to foreground)
+// 11. bg (move job to background)
+
+char *find_executable(char *command);
+
+static const char *parent_commands[] = {
+    "cd", "export", "unset", "exit", "source", ".", "alias", "unalias", "set", "jobs", "fg", "bg", NULL
 };
 
+static const char *builtin_parent_commands[] = {
+    "cd", "export", "unset", "exit", NULL
+};
+// Array of builtin command names
+static const char *builtin_commands[] = {
+    "cd", "pwd", "export", "echo", "unset", "env", "exit", "bye", NULL
+};
+
+// static const char *builtin_child_commands[] = {
+//     "pwd", "echo", "env", NULL
+// };
+
+int is_parent_command(char *command)
+{
+    // printf("DEBUG: Checking if '%s' is a builtin command\n", command);
+    int i = 0;
+    while (parent_commands[i])
+    {
+        // printf("DEBUG: Comparing '%s' with '%s'\n", command, builtin_commands[i]);
+        if (ft_strcmp(command, parent_commands[i]) == 0)
+        {
+            // printf("DEBUG: Found builtin command '%s'\n", command);
+            return 1;
+        }
+        i++;
+    }
+    // printf("DEBUG: '%s' is not a builtin command\n", command);
+    return 0;
+}
+
+int    is_builtin_parent(char *command)
+{
+    // printf("DEBUG: Checking if '%s' is a builtin command\n", command);
+    int i = 0;
+    while (builtin_parent_commands[i])
+    {
+        // printf("DEBUG: Comparing '%s' with '%s'\n", command, builtin_commands[i]);
+        if (ft_strcmp(command, builtin_parent_commands[i]) == 0)
+        {
+            // printf("DEBUG: Found builtin command '%s'\n", command);
+            return 1;
+        }
+        i++;
+    }
+    // printf("DEBUG: '%s' is not a builtin command\n", command);
+    return 0;
+}
+
+void    execute_external(t_command *cmd)
+{
+    char *path;
+
+    path = find_executable(cmd->argv[0]);
+    printf("external command: %s\n", path);
+    execve(path, cmd->argv, environ);
+    perror("minishell: execve failed\n");
+    exit(EXIT_FAILURE);
+}
+void execute_parent(t_command *cmd)
+{
+    if (is_builtin_parent(cmd->argv[0]))
+    {
+        execute_builtin(cmd);
+    }
+    else
+    {
+        execute_external(cmd);
+    }
+}
 
 int is_builtin(char *command)
 {
--- src/executor.c
+++ src/executor.c
@@ -6,36 +6,15 @@
 /*   By: eeklund <eeklund@student.42.fr>              +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/08/13 18:15:38 by eeklund       #+#    #+#                 */
-/*   Updated: 2024/08/21 11:06:36 by eeklund       ########   odam.nl         */
+/*   Updated: 2024/08/23 15:54:32 by rdl           ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+#include "builtins.h"
 
  void	print_sorted_env(void);
 
-// char *find_executable(char *command)
-// {
-//     char *path = getenv("PATH");
-//     char *path_copy = ft_strdup(path);
-//     //illegal strtok!!!
-//     char *dir = strtok(path_copy, ":");
-//     char *full_path = NULL;
-
-//     while (dir != NULL) {
-//         full_path = malloc(ft_strlen(dir) + ft_strlen(command) + 2);
-//         sprintf(full_path, "%s/%s", dir, command);
-//         if (access(full_path, X_OK) == 0) {
-//             free(path_copy);
-//             return full_path;
-//         }
-//         free(full_path);
-//         dir = strtok(NULL, ":");
-//     }
-
-//     free(path_copy);
-//     return NULL;
-// }
 char *find_executable(char *command)
 {
     char *path = getenv("PATH");
@@ -57,115 +36,6 @@ char *find_executable(char *command)
     return ft_strdup(command);  // Return the command as-is if not found in PATH
 }
 
-// void execute_command(char *command)
-// {
-//     t_command *cmd;
-//     pid_t pid;
-//     int status;
-
-//     cmd = parse_command(command);
-//     if (!cmd)
-//         return;
-
-//     if (is_builtin(cmd->argv[0]))
-//     {
-//         printf("Executing builtin command: %s\n", cmd->argv[0]);
-//         execute_builtin(cmd);
-//     }
-//     else
-//     {
-//         pid = fork();
-//         if (pid == -1)
-//         {
-//             perror("minishell: fork");
-//         }
-//         else if (pid == 0)
-//         {
-//             setup_redirections(cmd);
-//             char *executable_path = find_executable(cmd->argv[0]);
-//             if (executable_path)
-//             {   
-//                 printf("Executing external command: %s\n", executable_path);
-//                 if (execve(executable_path, cmd->argv, environ) == -1)
-//                 {
-//                     perror("minishell: execve failed");
-//                     free(executable_path);
-//                     exit(EXIT_FAILURE);
-//                 }
-//             }
-//             else
-//             {
-//                 fprintf(stderr, "minishell: command not found: %s\n", cmd->argv[0]);
-//                 exit(EXIT_FAILURE);
-//             }
-//         }
-//         else
-//         {
-//             waitpid(pid, &status, 0);
-//             update_exit_status(status);
-//         }
-//     }
-
-//     free_command(cmd);  // Make sure this is called only once
-// // }
-// void execute_command(char *command)
-// {
-//     t_command *cmd;
-//     pid_t pid;
-//     int status;
-
-//     cmd = parse_command(command);
-//     if (!cmd)
-//         return;
-
-//     if (is_builtin(cmd->argv[0]))
-//     {
-//         execute_builtin(cmd);
-//     }
-//     else
-//     {
-//         pid = fork();
-//         if (pid == -1)
-//         {
-//             perror("minishell: fork");
-//         }
-//         else if (pid == 0)
-//         {
-//             setup_redirections(cmd);
-//             char *executable_path = find_executable(cmd->argv[0]);
-//             if (executable_path)
-//             {
-//                 execve(executable_path, cmd->argv, environ);
-//                 perror("minishell: execve failed");
-//                 free(executable_path);
-//                 exit(EXIT_FAILURE);
-//             }
-//             else
-//             {
-//                 fprintf(stderr, "minishell: command not found: %s\n", cmd->argv[0]);
-//                 exit(EXIT_FAILURE);
-//             }
-//         }
-//         else
-//         {
-//             waitpid(pid, &status, 0);
-//             update_exit_status(status);
-//         }
-//     }
-
-//     free_command(cmd);
-// }
-
-// char *find_command_path(char *cmd_name)
-// {
-// 	char	*path;
-
-// 	path = getenv("PATH");
-// 	if (!path)
-// 		return (NULL);
-	
-// }
-
 void execute_command(char *command)
 {
 	t_token		*tokens;
@@ -186,8 +56,10 @@ void execute_command(char *command)
 		return ;
 	}
 	// print_cmd_list(cmd);
-	if (is_builtin(cmd->argv[0]))
-		execute_builtin(cmd);
+	if (is_parent_command(cmd->argv[0]))
+		execute_parent(cmd);
+	// if (is_builtin(cmd->argv[0])
+	// 	execute_builtin(cmd);
 	else
 	{
 		pid = fork();
@@ -204,6 +76,7 @@ void execute_command(char *command)
 			// 	builtin_echo(cmd->argv);
 			// }
 			path = find_executable(cmd->argv[0]);
+			printf("external command: %s\n", path);
 			execve(path, cmd->argv, environ);
 			perror("minishell: execve failed\n");
 			exit(EXIT_FAILURE);
