c320108e6819af429a421c25dde7244d2f170db8
Ellen Eklund <eeklund@f1r2s20.codam.nl>
2024-08-23 15:04:01 +0200

    variable expansion extra

---
+++ include/var_exp.h
@@ -0,0 +1,24 @@
+// /* ************************************************************************** */
+// /*                                                                            */
+// /*                                                        ::::::::            */
+// /*   var_exp.h                                          :+:    :+:            */
+// /*                                                     +:+                    */
+// /*   By: eeklund <eeklund@student.42.fr>              +#+                     */
+// /*                                                   +#+                      */
+// /*   Created: 2024/08/23 13:36:31 by eeklund       #+#    #+#                 */
+// /*   Updated: 2024/08/23 13:39:03 by eeklund       ########   odam.nl         */
+// /*                                                                            */
+// /* ************************************************************************** */
+
+// #ifndef VAR_EXP_H
+// # define VAR_EXP_H
+
+// # include "minishell.h"
+
+// void	variable_exp_double(t_token *token, char *str);
+// int		until_dollar(char *str);
+// void	variable_exp_dollar(t_token *token, char *str);
+// int		is_var_char(char c);
+// int		find_var_len(char *var);
+
+// #endif
