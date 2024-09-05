80c884b08f34c37baf903138df5b1ea602031503
Ellen Eklund <eeklund@f1r1s16.codam.nl>
2024-09-05 16:17:17 +0200

    fixed executor

--- src/builtins/builtins.c
+++ src/builtins/builtins.c
@@ -34,95 +34,99 @@ static const char *builtin_commands[] = {
 //     "pwd", "echo", "env", NULL
 // };
 
-int    is_builtin_parent(char *command)
+int	is_builtin_parent(char *command)
 {
-    // printf("DEBUG: Checking if '%s' is a builtin command\n", command);
-    int i = 0;
-    while (builtin_parent_commands[i])
-    {
-        // printf("DEBUG: Comparing '%s' with '%s'\n", command, builtin_commands[i]);
-        if (ft_strcmp(command, builtin_parent_commands[i]) == 0)
-        {
-            // printf("DEBUG: Found builtin command '%s'\n", command);
-            return 1;
-        }
-        i++;
-    }
-    // printf("DEBUG: '%s' is not a builtin command\n", command);
-    return 0;
+	// printf("DEBUG: Checking if '%s' is a builtin command\n", command);
+	int	i;
+
+	i = 0;
+	while (builtin_parent_commands[i])
+	{
+		// printf("DEBUG: Comparing '%s' with '%s'\n", command, builtin_commands[i]);
+		if (ft_strcmp(command, builtin_parent_commands[i]) == 0)
+		{
+			// printf("DEBUG: Found builtin command '%s'\n", command);
+			return 1;
+		}
+		i++;
+	}
+	// printf("DEBUG: '%s' is not a builtin command\n", command);
+	return 0;
 }
 
 void    execute_external(t_command *cmd)
 {
-    char *path;
+	char	*path;
 
-    path = find_executable(cmd->argv[0]);
-    // printf("external command: %s\n", path);
-    execve(path, cmd->argv, environ);
-    perror("minishell: execve failed\n");
-    exit(EXIT_FAILURE);
+	path = find_executable(cmd->argv[0]);
+	// printf("external command: %s\n", path);
+	execve(path, cmd->argv, environ);
+	perror("minishell: execve failed\n");
+	exit(EXIT_FAILURE);
 }
 
 int is_builtin(char *command)
 {
-    // printf("DEBUG: Checking if '%s' is a builtin command\n", command);
-    int i = 0;
-    while (builtin_commands[i])
-    {
-        // printf("DEBUG: Comparing '%s' with '%s'\n", command, builtin_commands[i]);
-        if (ft_strcmp(command, builtin_commands[i]) == 0)
-        {
-            // printf("DEBUG: Found builtin command '%s'\n", command);
-            return 1;
-        }
-        i++;
-    }
-    // printf("DEBUG: '%s' is not a builtin command\n", command);
-    return 0;
+	// printf("DEBUG: Checking if '%s' is a builtin command\n", command);
+	int i;
+
+	i = 0;
+	while (builtin_commands[i])
+	{
+		// printf("DEBUG: Comparing '%s' with '%s'\n", command, builtin_commands[i]);
+		if (ft_strcmp(command, builtin_commands[i]) == 0)
+		{
+			// printf("DEBUG: Found builtin command '%s'\n", command);
+			return (1);
+		}
+		i++;
+	}
+	// printf("DEBUG: '%s' is not a builtin command\n", command);
+	return 0;
 }
 
 void execute_builtin(t_command *cmd)
 {
-    // printf("DEBUG: Executing builtin command '%s'\n", cmd->argv[0]);
-    if (ft_strcmp(cmd->argv[0], "echo") == 0)
-    {
-        // printf("DEBUG: Executing 'echo' builtin\n");
-        builtin_echo(cmd->argv);
-    }
-    else if (ft_strcmp(cmd->argv[0], "cd") == 0)
-    {
-        // printf("DEBUG: Executing 'cd' builtin\n");
-        builtin_cd(cmd->argv);
-    }
-    else if (ft_strcmp(cmd->argv[0], "pwd") == 0)
-    {
-        // printf("DEBUG: Executing 'pwd' builtin\n");
-        builtin_pwd(cmd->argv);
-    }
-    else if (ft_strcmp(cmd->argv[0], "export") == 0)
-    {
-        // printf("DEBUG: Executing 'export' builtin\n");
-        builtin_export(cmd->argv);
-    }
-    else if (ft_strcmp(cmd->argv[0], "unset") == 0)
-    {
-        // printf("DEBUG: Executing 'unset' builtin\n");
-        builtin_unset(cmd->argv);
-    }
-    else if (ft_strcmp(cmd->argv[0], "env") == 0)
-    {
-        // printf("DEBUG: Executing 'env' builtin\n");
-        builtin_env(cmd->argv);
-    }
-    else if (ft_strcmp(cmd->argv[0], "exit") == 0)
-    {
-        // printf("DEBUG: Executing 'exit' builtin\n");
-        builtin_exit(cmd->argv);
-    }
-     else if (ft_strcmp(cmd->argv[0], "bye") == 0)
-    {
-        // printf("DEBUG: Executing 'exit' builtin\n");
-        builtin_exit(cmd->argv);
-    }
-    // printf("DEBUG: Finished executing builtin command '%s'\n", cmd->argv[0]);
+	// printf("DEBUG: Executing builtin command '%s'\n", cmd->argv[0]);
+	if (ft_strcmp(cmd->argv[0], "echo") == 0)
+	{
+		// printf("DEBUG: Executing 'echo' builtin\n");
+		builtin_echo(cmd->argv);
+	}
+	else if (ft_strcmp(cmd->argv[0], "cd") == 0)
+	{
+		// printf("DEBUG: Executing 'cd' builtin\n");
+		builtin_cd(cmd->argv);
+	}
+	else if (ft_strcmp(cmd->argv[0], "pwd") == 0)
+	{
+		// printf("DEBUG: Executing 'pwd' builtin\n");
+		builtin_pwd(cmd->argv);
+	}
+	else if (ft_strcmp(cmd->argv[0], "export") == 0)
+	{
+		// printf("DEBUG: Executing 'export' builtin\n");
+		builtin_export(cmd->argv);
+	}
+	else if (ft_strcmp(cmd->argv[0], "unset") == 0)
+	{
+		// printf("DEBUG: Executing 'unset' builtin\n");
+		builtin_unset(cmd->argv);
+	}
+	else if (ft_strcmp(cmd->argv[0], "env") == 0)
+	{
+		// printf("DEBUG: Executing 'env' builtin\n");
+		builtin_env(cmd->argv);
+	}
+	else if (ft_strcmp(cmd->argv[0], "exit") == 0)
+	{
+		// printf("DEBUG: Executing 'exit' builtin\n");
+		builtin_exit(cmd->argv);
+	}
+		else if (ft_strcmp(cmd->argv[0], "bye") == 0)
+	{
+		// printf("DEBUG: Executing 'exit' builtin\n");
+		builtin_exit(cmd->argv);
+	}
+	// printf("DEBUG: Finished executing builtin command '%s'\n", cmd->argv[0]);
 }
--- src/execution/redirections.c
+++ src/execution/redirections.c
@@ -6,57 +6,133 @@
 /*   By: eeklund <eeklund@student.42.fr>              +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/08/21 11:00:43 by eeklund       #+#    #+#                 */
-/*   Updated: 2024/09/04 15:42:18 by eeklund       ########   odam.nl         */
+/*   Updated: 2024/09/05 16:05:01 by eeklund       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-// NEEDS TO BE FIXED
-// flow of input/output, what should be redirected, also maybe not use dup here but wait til later
-// redircetions in combination with pipes is still fucked
+int	open_file(char *target, t_token_type type)
+{
+	int	fd;
+
+	if (type == TOKEN_REDIRECT_IN)
+		fd = open(target, O_RDONLY);
+	else if (type == TOKEN_REDIRECT_OUT)
+		fd = open(target, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	else if (type == TOKEN_REDIRECT_APPEND)
+		fd = open(target, O_WRONLY | O_CREAT | O_APPEND, 420);
+	return (fd);
+}
+
+void	handle_file_opening(int *fd, char *target, t_token_type type)
+{
+	if (*fd != -1)
+		close (*fd);
+	*fd = open_file(target, type);
+	if (*fd == -1)
+	{
+		perror("Error opening file for redirection");
+		exit(EXIT_FAILURE);
+	}
+
+}
+
+void	redirect_stream(int fd, t_token_type type)
+{
+	if (type == TOKEN_REDIRECT_IN)
+	{
+		if (dup2(fd, STDIN_FILENO) == -1)
+		{
+			perror("Error redirecting stdin");
+			close(fd);
+			exit(EXIT_FAILURE);
+		}
+		close (fd);
+	}
+	else if (type == TOKEN_REDIRECT_OUT || type == TOKEN_REDIRECT_APPEND)
+	{
+		if (dup2(fd, STDOUT_FILENO) == -1)
+		{
+			perror("Error redirecting stdout");
+			close(fd);
+			exit(EXIT_FAILURE);
+		}
+		close (fd);
+	}
+}
+
 void	setup_redirections(t_command *cmd)
 {
-	// int	fd;
 	int	cur;
 
 	cur = 0;
 	while (cur < cmd->redirect_count)
 	{
-		if (cmd->redir[cur].type == TOKEN_REDIRECT_OUT)
-			cmd->output = open(cmd->redir[cur].file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-		else if (cmd->redir[cur].type == TOKEN_REDIRECT_APPEND)
-			cmd->output = open(cmd->redir[cur].file, O_WRONLY | O_CREAT | O_APPEND, 420);
-		else if (cmd->redir[cur].type == TOKEN_REDIRECT_IN)
-			cmd->output = open(cmd->redir[cur].file, O_RDONLY); // unclear if it shoul be input or output
-		if (cmd->output == -1)
-		{
-			perror("Error opening file for redirection");
-			return ;
-		}
+		if (cmd->redir[cur].type == TOKEN_REDIRECT_IN)
+			handle_file_opening(&cmd->input, cmd->redir[cur].file, \
+			TOKEN_REDIRECT_IN);
+		else
+			if (cmd->redir[cur].type == TOKEN_REDIRECT_OUT || \
+			cmd->redir[cur].type == TOKEN_REDIRECT_APPEND)
+				handle_file_opening(&cmd->output, cmd->redir[cur].file, \
+				cmd->redir[cur].type);
 		cur++;
 	}
-	cur--;
-	if ((cmd->redir[cur].type == TOKEN_REDIRECT_OUT || \
-	cmd->redir[cur].type == TOKEN_REDIRECT_APPEND))
-	{
-		if (dup2(cmd->output, STDOUT_FILENO) == -1)
-		{
-			perror("Error redirecting stdout");
-			close(cmd->output);
-			return ;
-		}
-	}
-	else if (cmd->redir[cur].type == TOKEN_REDIRECT_IN)
-	{
-		if (dup2(cmd->output, STDIN_FILENO) == -1)
-		{
-			perror("Error redirecting stdout");
-			close(cmd->output);
-			return ;
-		}
-	}
-	close(cmd->output);
+	if (cmd->redir[cur - 1].type == TOKEN_REDIRECT_IN)
+		redirect_stream(cmd->input, cmd->redir[cur - 1].type);
+	else if (cmd->redir[cur - 1].type == TOKEN_REDIRECT_OUT || \
+	cmd->redir[cur - 1].type == TOKEN_REDIRECT_APPEND)
+		redirect_stream(cmd->output, cmd->redir[cur - 1].type);
+	// if (cmd->redir[cur]->type == TOKEN_HEREDOC)
+	// 	handle_heredoc(cmd);
+}
+
+// NEEDS TO BE FIXED
+// flow of input/output, what should be redirected, also maybe not use dup here but wait til later
+// redircetions in combination with pipes is still fucked
+// void	setup_redirections(t_command *cmd)
+// {
+// 	// int	fd;
+// 	int	cur;
+
+// 	cur = 0;
+// 	while (cur < cmd->redirect_count)
+// 	{
+// 		if (cmd->redir[cur].type == TOKEN_REDIRECT_OUT)
+// 			cmd->output = open(cmd->redir[cur].file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+// 		else if (cmd->redir[cur].type == TOKEN_REDIRECT_APPEND)
+// 			cmd->output = open(cmd->redir[cur].file, O_WRONLY | O_CREAT | O_APPEND, 420);
+// 		else if (cmd->redir[cur].type == TOKEN_REDIRECT_IN)
+// 			cmd->output = open(cmd->redir[cur].file, O_RDONLY); // unclear if it shoul be input or output
+// 		if (cmd->output == -1)
+// 		{
+// 			perror("Error opening file for redirection");
+// 			return ;
+// 		}
+// 		cur++;
+// 	}
+// 	cur--;
+// 	if ((cmd->redir[cur].type == TOKEN_REDIRECT_OUT || 
+// 	cmd->redir[cur].type == TOKEN_REDIRECT_APPEND))
+// 	{
+// 		if (dup2(cmd->output, STDOUT_FILENO) == -1)
+// 		{
+// 			perror("Error redirecting stdout");
+// 			close(cmd->output);
+// 			return ;
+// 		}
+// 	}
+// 	else if (cmd->redir[cur].type == TOKEN_REDIRECT_IN)
+// 	{
+// 		if (dup2(cmd->output, STDIN_FILENO) == -1)
+// 		{
+// 			perror("Error redirecting stdout");
+// 			close(cmd->output);
+// 			return ;
+// 		}
+// 	}
+// 	close(cmd->output);
 
 
 	// if (cmd->redirects->type == TOKEN_HEREDOC)
@@ -65,7 +141,7 @@ void	setup_redirections(t_command *cmd)
 	// }
 
 	// printf("DEBUG: setup_redirections called\n");
-}
+// }
 
 
 // if ((cur == cmd->redirect_count - 1)  && (cmd->redir[cur].type == 
