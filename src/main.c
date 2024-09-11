91d1ffd704c103d351a4715e6d9f98c14e27387d
roman lodeizen <rlodeize@student.codam.nl>
2024-09-11 18:32:42 +0200

    commented out readline block

--- src/main.c
+++ src/main.c
@@ -104,7 +104,6 @@ void minishell_loop(void)
     char *line;
     char *prompt;
     setup_signals_shell();
-    rl_catch_signals = 0; // Prevent readline from handling signals 
 
     while (1)
     {
