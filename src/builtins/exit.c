6577ff0c272e5e92d70719695798cde4f34f8f3c
roman lodeizen <rlodeize@student.codam.nl>
2024-09-21 16:48:49 +0200

    only unset Path to be fixed

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -1,67 +1,3 @@
-// #include <limits.h>
-// #include <stdio.h>
-// #include <stdlib.h>
-
-// static int is_space(char c) 
-// {
-//     return (c == ' ' || (c >= '\t' && c <= '\r'));
-// }
-
-// static int is_digit(char c) 
-// {
-//     return (c >= '0' && c <= '9');
-// }
-
-// int is_valid_number(const char *str, long *number) 
-// {
-//     int i = 0;
-//     int sign = 1;
-//     unsigned long result = 0;
-
-//     while (is_space(str[i])) 
-//         i++;
-//     if (str[i] == '+' || str[i] == '-') 
-// 	{
-//         if (str[i] == '-') 
-//             sign = -1;
-//         i++;
-//     }
-//     if (!is_digit(str[i])) 
-//         return 0; // Invalid number
-//     while (str[i] != '\0') {
-//         if (!is_digit(str[i]))
-//             return 0; // Non-digit character found
-//         int digit = str[i] - '0';
-//         if (result > (ULONG_MAX - digit) / 10)
-//             return 0; // Overflow detected
-//         result = result * 10 + digit;
-//         i++;
-//     }
-//      if (sign == -1 && result > (unsigned long)LONG_MAX + 1) 
-//         return 0; // Underflow detected
-//     if (sign == 1 && result > (unsigned long)LONG_MAX)
-//         return 0; // Overflow detected
-//     *number = sign * (long)result;
-//     return 1; // Valid number
-// }
-
-// void builtin_exit(char **args) {
-//     long exit_status;
-
-//     printf("exit\n");
-//     if (!args[1]) {
-//         exit(0);
-//     }
-//     if (args[2]) {
-//         fprintf(stderr, "minishell: exit: too many arguments\n");
-//         return;
-//     }
-//     if (!is_valid_number(args[1], &exit_status)) {
-//         fprintf(stderr, "minishell: exit: %s: numeric argument required\n", args[1]);
-//         exit(255);
-//     }
-//     exit((int)(exit_status & 255));
-// }
 
 #include <limits.h>
 #include <stdio.h>
@@ -150,7 +86,7 @@ void builtin_exit(char **args)
     {
         // Handle non-numeric argument error, follow Bash behavior and return 2 for invalid input
         fprintf(stderr, "minishell: exit: %s: numeric argument required\n", args[1]);
-        exit(255);  // This matches Bash's behavior for invalid numeric arguments
+        exit(2);  // Changed from 255 to 2 to match the desired behavior
     }
 
     // Exit with the parsed numeric value (masked by 255 to mimic Bash)
