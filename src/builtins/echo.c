82599f7ba41d8c4f6e515439c39acf799cef0ea2
roman lodeizen <rlodeize@student.codam.nl>
2024-09-21 16:40:20 +0200

    fianlly fixed the quote removal the hard waygit status

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -6,7 +6,36 @@
 
 static bool	is_only_n_flag(char *arg);
 void 		builtin_echo(char **args);
-
+// make norm proof!!!
+static char* remove_paired_quotes(const char* input) {
+    int len = strlen(input);
+    char* output = malloc(len + 1);  // Allocate memory for the worst case
+    int i = 0, j = 0;
+    
+    while (i < len) {
+        if (input[i] == '\'' || input[i] == '\"') {
+            char quote = input[i];
+            int end = i + 1;
+            while (end < len && input[end] != quote) {
+                end++;
+            }
+            if (end < len) {  // Matching quote found
+                // Copy content between quotes
+                memcpy(output + j, input + i + 1, end - i - 1);
+                j += end - i - 1;
+                i = end + 1;
+            } else {
+                // No matching quote, treat as normal character
+                output[j++] = input[i++];
+            }
+        } else {
+            output[j++] = input[i++];
+        }
+    }
+    
+    output[j] = '\0';  // Null-terminate the string
+    return output;
+}
 
 static bool	is_only_n_flag(char *arg)
 {
@@ -26,24 +55,24 @@ static bool	is_only_n_flag(char *arg)
 
 void builtin_echo(char **args)
 {
-	int i = 1;
-	bool newline = true;
-	if (!args)
-		return;
-	while (args[i] && is_only_n_flag(args[i]))
-	{	
-		newline = false;
-		i++;
-	}
-	// remove_quotes(args[i]);
-	while (args[i])
-	{	
-		// remove_quotes(args[i]);
-		printf("%s", args[i]);
-		if (args[i + 1])
-			printf(" ");
-		i++;
-	}
-	if (newline)
-		printf("\n");
+    int i = 1;
+    bool newline = true;
+    if (!args)
+        return;
+    while (args[i] && is_only_n_flag(args[i]))
+    {   
+        newline = false;
+        i++;
+    }
+    while (args[i])
+    {   
+        char *processed = remove_paired_quotes(args[i]);
+        printf("%s", processed);
+        free(processed);  // Don't forget to free the allocated memory
+        if (args[i + 1])
+            printf(" ");
+        i++;
+    }
+    if (newline)
+        printf("\n");
 }
