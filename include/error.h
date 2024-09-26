06bc918bb71edc9fc6736511717d52fb2ac8fb84
roman lodeizen <rlodeize@student.codam.nl>
2024-09-26 19:25:29 +0200

    deleted unused functions declarations

--- include/error.h
+++ include/error.h
@@ -6,7 +6,7 @@
 /*   By: eeklund <eeklund@student.42.fr>              +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/09/17 13:42:15 by eeklund       #+#    #+#                 */
-/*   Updated: 2024/09/26 17:28:06 by eeklund       ########   odam.nl         */
+/*   Updated: 2024/09/26 19:25:14 by rdl           ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,23 +23,5 @@ void	print_command_not_found(char *command);
 
 // Function to print error for invalid exit arguments
 void	print_exit_numeric_error(char *arg);
-
-// Function to handle unset PATH error
-void	print_unset_path_error(void);
-
-// Function to handle file/directory not found errors
-void	print_file_not_found_error(char *path);
-
-// General function for handling permission errors
-void	print_permission_error(char *path);
-
-// Function for handling syntax errors (like unexpected tokens)
-void	print_syntax_error(char *token);
-
-// Function to print generic errors
-void	print_generic_error(char *msg);
-
-// Function for handling memory allocation errors
-void	handle_memory_error(void);
 
 #endif /* ERROR_H */
