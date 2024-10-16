7034187c0c4274fc630fb8ef6f9aea2dd173b860
roman lodeizen <rlodeize@student.codam.nl>
2024-10-16 17:21:53 +0200

    finafucking lee

--- src/builtins/export.c
+++ src/builtins/export.c
@@ -18,16 +18,18 @@ static void	sort_env(char **sorted_env, int size);
 static int	print_sorted_env(t_shell *shell);
 void		builtin_export(char **args, t_shell *shell);
 
-static void	print_export_var(char *var)
+static void print_export_var(char *var)
 {
-	char	*e_sign;
-
-	e_sign = ft_strchr(var, '=');
-	if (e_sign)
-		printf("declare -x %.*s\"%s\"\n", (int)(e_sign - var + 1),
-			var, e_sign + 1);
-	else
-		printf("declare -x %s\n", var);
+    char *equal_sign = ft_strchr(var, '=');
+    
+    if (equal_sign)
+    {
+        printf("declare -x %.*s\"%s\"\n", (int)(equal_sign - var + 1), var, equal_sign + 1);
+    }
+    else
+    {
+        printf("declare -x %s\n", var);
+    }
 }
 
 static void	sort_env(char **sorted_env, int size)
@@ -52,50 +54,103 @@ static void	sort_env(char **sorted_env, int size)
 	}
 }
 
-// static int	print_sorted_env(t_shell *shell)
-// {
-// 	char	**sorted_env;
-// 	int		i;
-// 	char	*e_sign;
+static int is_in_export_list(char *var, t_shell *shell)
+{
+    int i;
+    char *equal_sign = ft_strchr(var, '=');
+    size_t len = equal_sign ? (size_t)(equal_sign - var) : ft_strlen(var);
 
-// 	sorted_env = malloc(sizeof(char *) * (shell->env_size + 1));
-// 	if (!sorted_env)
-// 		return (0);
-// 	i = -1;
-// 	while (++i < shell->env_size)
-// 		sorted_env[i] = shell->env[i];
-// 	sorted_env[i] = NULL;
-// 	sort_env(sorted_env, shell->env_size);
-// 	i = -1;
-// 	while (++i < shell->env_size)
-// 	{
-// 		e_sign = ft_strchr(sorted_env[i], '=');
-// 		printf("declare -x %.*s\"%s\"\n", (int)(e_sign - sorted_env[i] + 1),
-// 			sorted_env[i], e_sign + 1);
-// 	}
-// 	free(sorted_env);
-// 	return (1);
-// }
+    for (i = 0; i < shell->export_size; i++)
+    {
+        if (ft_strncmp(shell->export_list[i], var, len) == 0
+            && (shell->export_list[i][len] == '=' || shell->export_list[i][len] == '\0'))
+            return (1);
+    }
+    return (0);
+}
+static void	print_and_free_env(char **combined_env, int combined_size)
+{
+	int	i;
 
+	i = -1;
+	while (++i < combined_size)
+	{
+		print_export_var(combined_env[i]);
+		free(combined_env[i]);
+	}
+	free(combined_env);
+}
 static int	print_sorted_env(t_shell *shell)
 {
-	char	**sorted_env;
+	char	**combined_env;
+	int		combined_size;
 	int		i;
+	int		j;
 
-	sorted_env = malloc(sizeof(char *) * (shell->export_size + 1));
-	if (!sorted_env)
+	combined_size = shell->export_size + shell->env_size;
+	combined_env = malloc(sizeof(char *) * (combined_size + 1));
+	if (!combined_env)
 		return (0);
 	i = -1;
 	while (++i < shell->export_size)
-		sorted_env[i] = shell->export_list[i];
-	sorted_env[i] = NULL;
-	sort_env(sorted_env, shell->export_size);
+		combined_env[i] = ft_strdup(shell->export_list[i]);
 	i = -1;
-	while (++i < shell->export_size)
-		print_export_var(sorted_env[i]);
-	free(sorted_env);
+	j = shell->export_size;
+	while (++i < shell->env_size)
+	{
+		if (!is_in_export_list(shell->env[i], shell))
+			combined_env[j++] = ft_strdup(shell->env[i]);
+	}
+	combined_env[j] = NULL;
+	combined_size = j;
+	sort_env(combined_env, combined_size);
+	print_and_free_env(combined_env, combined_size);
 	return (1);
 }
+
+
+// static int print_sorted_env(t_shell *shell)
+// {
+//     char **combined_env;
+//     int combined_size;
+//     int i, j;
+
+//     // Calculate the total size (export_list + env)
+//     combined_size = shell->export_size + shell->env_size;
+
+//     // Allocate memory for the combined list
+//     combined_env = malloc(sizeof(char *) * (combined_size + 1));
+//     if (!combined_env)
+//         return (0);
+
+//     // Copy export_list
+//     for (i = 0; i < shell->export_size; i++)
+//         combined_env[i] = ft_strdup(shell->export_list[i]);
+
+//     // Copy env (excluding duplicates)
+//     for (i = 0, j = shell->export_size; i < shell->env_size; i++)
+//     {
+//         if (!is_in_export_list(shell->env[i], shell))
+//             combined_env[j++] = ft_strdup(shell->env[i]);
+//     }
+
+//     combined_env[j] = NULL;
+//     combined_size = j;
+
+//     // Sort the combined list
+//     sort_env(combined_env, combined_size);
+
+//     // Print the sorted list
+//     for (i = 0; i < combined_size; i++)
+//         print_export_var(combined_env[i]);
+
+//     // Free the combined list
+//     for (i = 0; i < combined_size; i++)
+//         free(combined_env[i]);
+//     free(combined_env);
+
+//     return (1);
+// }
 
 void	builtin_export(char **args, t_shell *shell)
 {
--- src/builtins/export_utils.c
+++ src/builtins/export_utils.c
@@ -1,211 +1,256 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        ::::::::            */
-/*   export_utils.c                                     :+:    :+:            */
-/*                                                     +:+                    */
-/*   By: eeklund <eeklund@student.42.fr>              +#+                     */
-/*                                                   +#+                      */
-/*   Created: 2024/10/14 14:27:43 by eeklund       #+#    #+#                 */
-/*   Updated: 2024/10/14 17:41:26 by eeklund       ########   odam.nl         */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "minishell.h"
 
-// static char	*get_concatenated_value(const char *existing_value,
-// 				const char *new_value);
-// static int	handle_append_assignment(char *arg, char *plus_equal_sign,
-// 				t_shell *shell);
-// static int	handle_value_assignment(char *arg, char *equal_sign,
-// 				t_shell *shell);
-// static int	set_environment(char *arg, char *equal_sign, t_shell *shell);
-// int			process_identifier(char *arg, char *equal_sign, t_shell *shell);
 
-// static char	*get_concatenated_value(const char *existing_value,
-// 		const char *new_value)
-// {
-// 	char	*result;
-// 	size_t	total_length;
+static int add_to_export_list(char *var, t_shell *shell);
+static int set_environment(char *arg, char *equal_sign, t_shell *shell);
+static int process_environment(char *name, char *value, int append_mode, t_shell *shell);
+static int find_export_index(char *name, t_shell *shell);
+static char *create_new_value(char *name, char *value, int append_mode, t_shell *shell);
+static int update_environment(char *name, char *new_value, int index, t_shell *shell);
+static void restore_equal_sign(char *equal_sign, int append_mode);
+int process_identifier(char *arg, char *equal_sign, t_shell *shell);
 
-// 	if (!existing_value)
-// 		existing_value = "";
-// 	total_length = ft_strlen(existing_value) + ft_strlen(new_value) + 1;
-// 	result = malloc(total_length);
-// 	if (!result)
-// 		return (NULL);
-// 	ft_strlcpy(result, existing_value, total_length);
-// 	ft_strlcat(result, new_value, total_length);
-// 	return (result);
-// }
 
-// static int	handle_append_assignment(char *arg,
-// 	char *equal_sign, t_shell *shell)
-// {
-// 	char	*identifier;
-// 	char	*new_value;
-// 	char	*existing_value;
-// 	char	*concatenated_value;
-// 	int		result;
-
-// 	identifier = NULL;
-// 	identifier = ft_strndup(arg, (equal_sign - arg) - 1);
-// 	if (!identifier)
-// 		return (0);
-// 	new_value = equal_sign + 1;
-// 	existing_value = ft_get_env(identifier, shell);
-// 	concatenated_value = get_concatenated_value(existing_value, new_value);
-// 	if (!concatenated_value)
-// 	{
-// 		free(identifier);
-// 		return (0);
-// 	}
-// 	result = ft_set_env(identifier, concatenated_value, shell);
-// 	free(concatenated_value);
-// 	free(identifier);
-// 	return (result);
-// }
-
-// static int	handle_value_assignment(char *arg, char *equal_sign, t_shell *shell)
-// {
-// 	char	*value;
-// 	char	*plus_sign;
-
-// 	plus_sign = equal_sign - 1;
-// 	if (plus_sign >= arg && *plus_sign == '+')
-// 	{
-// 		return (handle_append_assignment(arg, equal_sign, shell));
-// 	}
-// 	*equal_sign = '\0';
-// 	value = equal_sign + 1;
-// 	if (!ft_set_env(arg, value, shell))
-// 		return (0);
-// 	*equal_sign = '=';
-// 	return (1);
-// }
-
-// static int	set_environment(char *arg, char *equal_sign, t_shell *shell)
-// {
-// 	if (equal_sign != NULL)
-// 	{
-// 		return (handle_value_assignment(arg, equal_sign, shell));
-// 	}
-// 	else
-// 	{
-// 		return (ft_set_env(arg, "", shell));
-// 	}
-// }
-
-// int	process_identifier(char *arg, char *equal_sign, t_shell *shell)
-// {
-// 	char	*identifier;
-// 	int		result;
-
-// 	if (equal_sign != NULL)
-// 	{
-// 		if (!handle_equal_sign(arg, equal_sign, &identifier))
-// 		{
-// 			shell->last_exit_status = 1;
-// 			return (0);
-// 		}
-// 	}
-// 	else
-// 		handle_no_equal_sign(arg, &identifier);
-// 	if (!validate_identifier(identifier, arg, shell))
-// 	{
-// 		if (equal_sign != NULL)
-// 			free(identifier);
-// 		return (0);
-// 	}
-// 	result = set_environment(arg, equal_sign, shell);
-// 	if (!result)
-// 		shell->last_exit_status = 1;
-// 	if (equal_sign != NULL)
-// 		free(identifier);
-// 	return (result);
-// }
-#include "minishell.h"
-
-static int	add_to_export_list(char *var, t_shell *shell)
+static int add_to_export_list(char *var, t_shell *shell)
 {
-	char	**new_list;
-	int		i;
+    char **new_list;
+    int i;
 
-	new_list = malloc(sizeof(char *) * (shell->export_size + 2));
-	if (!new_list)
-		return (0);
-	i = -1;
-	while (++i < shell->export_size)
-		new_list[i] = shell->export_list[i];
-	new_list[i] = ft_strdup(var);
-	new_list[i + 1] = NULL;
-	free(shell->export_list);
-	shell->export_list = new_list;
-	shell->export_size++;
-	return (1);
+    new_list = malloc(sizeof(char *) * (shell->export_size + 2));
+    if (!new_list)
+        return (0);
+    i = -1;
+    while (++i < shell->export_size)
+        new_list[i] = shell->export_list[i];
+    new_list[i] = ft_strdup(var);
+    new_list[i + 1] = NULL;
+    free(shell->export_list);
+    shell->export_list = new_list;
+    shell->export_size++;
+    return (1);
 }
-
-static int	is_in_export_list(char *var, t_shell *shell)
-{
-	int		i;
-	size_t	len;
-
-	len = ft_strlen(var);
-	i = -1;
-	while (++i < shell->export_size)
-	{
-		if (ft_strncmp(shell->export_list[i], var, len) == 0
-			&& (shell->export_list[i][len] == '='
-			|| shell->export_list[i][len] == '\0'))
-			return (1);
-	}
-	return (0);
-}
-
-static int	set_env_with_equal(char *arg, char *equal_sign, t_shell *shell)
-{
-	*equal_sign = '\0';
-	if (!ft_set_env(arg, equal_sign + 1, shell))
-		return (0);
-	*equal_sign = '=';
-	if (!is_in_export_list(arg, shell))
-		return (add_to_export_list(arg, shell));
-	return (1);
-}
-
 static int	set_environment(char *arg, char *equal_sign, t_shell *shell)
 {
-	if (equal_sign != NULL)
-		return (set_env_with_equal(arg, equal_sign, shell));
-	else if (!is_in_export_list(arg, shell))
-		return (add_to_export_list(arg, shell));
+	char	*name;
+	char	*value;
+	int		append_mode;
+
+	name = arg;
+	value = NULL;
+	append_mode = 0;
+	if (equal_sign)
+	{
+		*equal_sign = '\0';
+		value = equal_sign + 1;
+		if (equal_sign > arg && *(equal_sign - 1) == '+')
+		{
+			append_mode = 1;
+			*(equal_sign - 1) = '\0';
+		}
+	}
+	if (!process_environment(name, value, append_mode, shell))
+		return (0);
+	restore_equal_sign(equal_sign, append_mode);
 	return (1);
 }
 
-int	process_identifier(char *arg, char *equal_sign, t_shell *shell)
+static int	process_environment(char *name, char *value, int append_mode,
+								t_shell *shell)
 {
-	char	*identifier;
-	int		result;
+	int		index;
+	char	*new_value;
 
-	if (equal_sign != NULL)
+	index = find_export_index(name, shell);
+	if (value)
 	{
-		if (!handle_equal_sign(arg, equal_sign, &identifier))
+		new_value = create_new_value(name, value, append_mode, shell);
+		if (!new_value || !update_environment(name, new_value, index, shell))
 		{
-			shell->last_exit_status = 1;
+			free(new_value);
 			return (0);
 		}
+		free(new_value);
 	}
-	else
-		handle_no_equal_sign(arg, &identifier);
-	if (!validate_identifier(identifier, arg, shell))
+	else if (index == shell->export_size)
 	{
-		if (equal_sign != NULL)
-			free(identifier);
-		return (0);
+		if (!add_to_export_list(name, shell))
+			return (0);
 	}
-	result = set_environment(arg, equal_sign, shell);
-	if (!result)
-		shell->last_exit_status = 1;
-	if (equal_sign != NULL)
-		free(identifier);
-	return (result);
+	return (1);
+}
+
+static int	find_export_index(char *name, t_shell *shell)
+{
+	int	i;
+	int	name_len;
+
+	i = 0;
+	name_len = ft_strlen(name);
+	while (i < shell->export_size)
+	{
+		if (ft_strncmp(shell->export_list[i], name, name_len) == 0
+			&& (shell->export_list[i][name_len] == '='
+				|| shell->export_list[i][name_len] == '\0'))
+			return (i);
+		i++;
+	}
+	return (i);
+}
+
+static char	*create_new_value(char *name, char *value, int append_mode,
+							t_shell *shell)
+{
+	char	*old_value;
+	char	*new_value;
+
+	old_value = ft_get_env(name, shell);
+	if (append_mode && old_value)
+		new_value = ft_strjoin(old_value, value);
+	else
+		new_value = ft_strdup(value);
+	return (new_value);
+}
+
+static int	update_environment(char *name, char *new_value, int index,
+								t_shell *shell)
+{
+	char	*temp;
+
+	if (!ft_set_env(name, new_value, shell))
+		return (0);
+	if (index < shell->export_size)
+		free(shell->export_list[index]);
+	else
+	{
+		shell->export_size++;
+		shell->export_list = realloc(shell->export_list,
+				sizeof(char *) * (shell->export_size + 1));
+		if (!shell->export_list)
+			return (0);
+		shell->export_list[shell->export_size] = NULL;
+	}
+	shell->export_list[index] = ft_strjoin(name, "=");
+	temp = ft_strjoin(shell->export_list[index], new_value);
+	free(shell->export_list[index]);
+	shell->export_list[index] = temp;
+	return (1);
+}
+
+static void	restore_equal_sign(char *equal_sign, int append_mode)
+{
+	if (equal_sign)
+	{
+		*equal_sign = '=';
+		if (append_mode)
+			*(equal_sign - 1) = '+';
+	}
+}
+
+// static int set_environment(char *arg, char *equal_sign, t_shell *shell)
+// {
+//     char *name, *value;
+//     int i, append_mode = 0;
+
+//     name = arg;
+//     if (equal_sign)
+//     {
+//         *equal_sign = '\0';
+//         value = equal_sign + 1;
+//         if (equal_sign > arg && *(equal_sign - 1) == '+')
+//         {
+//             append_mode = 1;
+//             *(equal_sign - 1) = '\0';
+//         }
+//     }
+//     else
+//         value = NULL;
+//     for (i = 0; i < shell->export_size; i++)
+//     {
+//         if (ft_strncmp(shell->export_list[i], name, ft_strlen(name)) == 0
+//             && (shell->export_list[i][ft_strlen(name)] == '='
+//                 || shell->export_list[i][ft_strlen(name)] == '\0'))
+//         {
+//             break;
+//         }
+//     }
+//     if (value != NULL)
+//     {
+//         char *old_value = ft_get_env(name, shell);
+//         char *new_value;
+
+//         if (append_mode && old_value)
+//         {
+//             new_value = ft_strjoin(old_value, value);
+//         }
+//         else
+//         {
+//             new_value = ft_strdup(value);
+//         }
+
+//         if (!ft_set_env(name, new_value, shell))
+//         {
+//             free(new_value);
+//             return 0;
+//         }
+//         if (i < shell->export_size)
+//         {
+//             free(shell->export_list[i]);
+//         }
+//         else
+//         {
+//             shell->export_size++;
+//             shell->export_list = realloc(shell->export_list, sizeof(char *) * (shell->export_size + 1));
+//             shell->export_list[shell->export_size] = NULL;
+//         }
+
+//         shell->export_list[i] = ft_strjoin(name, "=");
+//         char *temp = ft_strjoin(shell->export_list[i], new_value);
+//         free(shell->export_list[i]);
+//         shell->export_list[i] = temp;
+
+//         free(new_value);
+//     }
+//     else if (i == shell->export_size)
+//     {
+//         if (!add_to_export_list(name, shell))
+//             return 0;
+//     }
+//     if (equal_sign)
+//     {
+//         *equal_sign = '=';
+//         if (append_mode)
+//             *(equal_sign - 1) = '+';
+//     }
+//     return 1;
+// }
+
+int process_identifier(char *arg, char *equal_sign, t_shell *shell)
+{
+    char *identifier;
+    int result;
+
+    if (equal_sign != NULL)
+    {
+        if (!handle_equal_sign(arg, equal_sign, &identifier))
+        {
+            shell->last_exit_status = 1;
+            return (0);
+        }
+    }
+    else
+        handle_no_equal_sign(arg, &identifier);
+
+    if (!validate_identifier(identifier, arg, shell))
+    {
+        if (equal_sign != NULL)
+            free(identifier);
+        return (0);
+    }
+    result = set_environment(arg, equal_sign, shell);
+    if (!result)
+        shell->last_exit_status = 1;
+
+    if (equal_sign != NULL)
+        free(identifier);
+    return (result);
 }
