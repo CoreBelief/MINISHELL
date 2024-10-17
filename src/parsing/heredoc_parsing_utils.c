a13c11ddd28e3e0dbc63031595ec5240cf877324
roman lodeizen <rlodeize@student.codam.nl>
2024-10-17 18:47:32 +0200

    heredoccerfokker

--- src/parsing/heredoc_parsing_utils.c
+++ src/parsing/heredoc_parsing_utils.c
@@ -55,73 +55,37 @@ int write_to_hdfd(char *delim, t_shell *shell, int hdfd)
     while (1)
     {
         if (isatty(STDIN_FILENO)) {
-            // Interactive mode: use readline
             content = readline("> ");
-        } else {
-            // Non-interactive mode: use get_next_line or fgets
-            content = get_next_line(STDIN_FILENO);  // Adjust this line based on what you're using
+        } else
+		{
+            content = get_next_line(STDIN_FILENO);
         }
-
-        if (!content) {
+        if (!content)
+		{
             write(STDERR_FILENO, "warning: here-document delimited by EOF\n", 40);
             break;
         }
-
-        if (ft_strcmp(content, delim) == 0) {
+        if (ft_strcmp(content, delim) == 0)
+		{
             free(content);
             break;
         }
-
         expansion = variable_exp_double(content, shell);
         free(content);
         if (!expansion) {
             close(hdfd);
             return (0);
         }
-
-        // Only add newline if in interactive mode
         write(hdfd, expansion, ft_strlen(expansion));
-
-        // Add newline only in interactive mode since get_next_line() may already add it
-        if (isatty(STDIN_FILENO)) {
+        if (isatty(STDIN_FILENO))
+		{
             write(hdfd, "\n", 1);
         }
-
         free(expansion);
     }
     close(hdfd);
     return (1);
 }
-
-
-// int	write_to_hdfd(char *delim, t_shell *shell, int hdfd)
-// {
-// 	char	*content;
-// 	char	*expansion;
-
-// 	while (1)
-// 	{
-// 		content = readline("> ");
-// 		if (!content)
-// 			break ;
-// 		if (ft_strcmp(content, delim) == 0)
-// 		{
-// 			free(content);
-// 			break ;
-// 		}
-// 		expansion = variable_exp_double(content, shell);
-// 		free(content);
-// 		if (!expansion)
-// 		{
-// 			return (0);
-// 		}
-// 		write(hdfd, expansion, ft_strlen(expansion));
-// 		write(hdfd, "\n", 1);
-// 		free(expansion);
-// 	}
-// 	close(hdfd);
-// 	return (1);
-// }
 
 int	validate_heredoc_syntax(t_cmd *cmd, t_token **token, t_shell *shell)
 {
